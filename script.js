0e5b77feea41e1fb4d7ef299aadbfc9964a18212
adams <adamskiptalam0@gmail.com>
2024-10-22 16:38:37 +0300

    Saving changes before applying patch

---
+++ script.js
@@ -0,0 +1,28 @@
+const textInput = document.getElementById('text-input');
+const checkBtn = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+
+function isPalindrome() {
+    const inputText = textInput.value.trim();
+
+    if (!inputText) {
+        alert("Please input a value");
+        return;
+    }
+
+    const cleanedText = inputText.replace(/[^A-Za-z0-9]/gi, "").toLowerCase();
+    const reversedText = cleanedText.split("").reverse().join("");
+    
+    //display result
+    if (reversedText === cleanedText) {
+        result.innerHTML = `<span class="bold">${inputText}</span> is a palindrome`;
+    } else {
+        result.innerHTML = `<span class="bold">${inputText}</span> is not a palindrome`;
+    }
+
+    // Clear the input field after checking
+    textInput.value = '';
+}
+
+checkBtn.addEventListener('click', isPalindrome);
